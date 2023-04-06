a4fb75ca5e7a1e3fb4207b5246cb26d76f06a4bc
Aryan Kumar <aryankumar877@gmail.com>
2023-04-06 20:00:52 +0530

    intial wireframe

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,37 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import '@/styles/globals.css';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import * as React from 'react';
+import createEmotionCache from '../createEmotionCache';
+import theme from '../theme';
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+export default function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta
+          name='viewport'
+          content='initial-scale=1, width=device-width'
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon.
+        remove the margins of all browsers and apply the material design background color */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
